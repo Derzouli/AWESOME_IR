63c0b790ea9a7efbc3f3ebd09cb351d6a5d26aae
lpoujade <lpoujade@student.42.fr>
2016-05-25 19:02:23 +0200

    /!\ on avait zappe un bout de trame (header / trame / tail qqes centaines de ms plus loin)
    le bout de code dans l interrupt allume et eteint la led d abord sur le
    header, puis fait pareil sur la trame mais en enregistrant dans un tableau
    PAR CONTRE pour l instant a cause de la fin de trame que j ai pas pris en
    compte ca finit bloque dans l interrupt et a part la premiere trame ce
    sera decale completement
    
    faudrait un bout de code qui prend les donnees du csv et transforme ca en
    nombre binaire, et on est bon pour l'IR :)

--- AwesomeCode.X/main.c
+++ AwesomeCode.X/main.c
@@ -8,8 +8,7 @@
 
 # include "fcts.h"
 
-u32 reception[50];
-u8  static_index = 0;
+u32 reception[32];
 
 // value for NEC
 // 560 usec
@@ -25,61 +24,33 @@ u8  static_index = 0;
 	__attribute__((interrupt(IPL4AUTO), nomips16, vector(_EXTERNAL_2_VECTOR)))
 void			ir_receive(void)
 {
-            u32 header;
-            u32 h_down;
-            u32 h_up;
-            u32 i;
+    u8 c = 0;
+    u32 tmr_tmp;
 
-        header = 0;
-        i = 0;
-        h_down = 0;
-        IFS0bits.T3IF = 0;
-	TMR2 = 0;
-        LATFbits.LATF1 = 1;
-        while (IFS0bits.T3IF == 0)
-        {
-            if (PORTDbits.RD9 == 0 && TMR2 < 360000) // down 0
-              header |= 1;
-            else if (PORTDbits.RD9 == 1 && TMR2 < 360000 && TMR2 < 539000) // up 0
-              header |= 2;
-            if (PORTDbits.RD9 == 0 && (TMR2 > 540000 && TMR2 < 562400))
-                h_down |= 1;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 && TMR2 < 562400 + 22400))
-                h_down |= 4;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 * 2 && TMR2 < 562400 + 22400 * 2))
-                h_down |= 8;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 * 3 && TMR2 < 562400 + 22400 * 3))
-                h_down |= 16;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 * 4 && TMR2 < 562400 + 22400 * 4))
-                h_down |= 32;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 * 5 && TMR2 < 562400 + 22400 * 5))
-                h_down |= 64;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 * 6 && TMR2 < 562400 + 22400 * 6))
-                h_down |= 128;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 * 7 && TMR2 < 562400 + 22400 * 7))
-                h_down |= 256;
-            else if (PORTDbits.RD9 == 0 && (TMR2 > 540000 + 22400 * 8 && TMR2 < 562400 + 22400 * 8))
-                h_down |= 512;
-        }
-        uart_putstr("\r\n");
-        uart_putnbr(h_down, 10);
-        uart_putstr("\r\n");
+    IFS0bits.T3IF = 0;
+    TMR2 = 0;
+    LATFbits.LATF1 = 1;
+    while (!PORTDbits.RD9)  // wait the header (1st bit, low signal)
+        ;
+    LATFbits.LATF1 = 0;
+    while (PORTDbits.RD9)   // header
+        ;
+    LATFbits.LATF1 = 1;
+    while (IFS0bits.T3IF == 0)  // lets get the trame
+    {
+        while (!PORTDbits.RD9)
+            ;
         LATFbits.LATF1 = 0;
-        IFS0bits.INT2IF = 0;
+        tmr_tmp = TMR2;
+        while (PORTDbits.RD9)
+            ;
+        LATFbits.LATF1 = 1;
+        reception[c] = TMR2 - tmr_tmp;
+        c++;
+    }
+    LATFbits.LATF1 = 0;
+    IFS0bits.INT2IF = 0;
 }
-
-/* Interrupt called each two captured event by Input Capture module.
- * Two events correspond to a falling edge then a rising (TSOP38238 default state is 1 - 3.3V)
- * so interrupt called each bits : if there's probably a way to be late (Input Capture buffer is 4x32 bits),'
- * its more simple if this routine take less than 167 micro seconds (minimum delay between two bits)
- */
-//__attribute__((interrupt(IPL4AUTO), nomips16, vector(_INPUT_CAPTURE_2_VECTOR)))
-//void    incap(void)
-//{
-//    reception[static_index] = IC2BUF;
-//    static_index++;
-//    IFS0bits.IC2IF = 0;
-//}
 
 int main(void)
 {
@@ -93,14 +64,13 @@ int main(void)
 
         IPC2bits.INT2IP = 4; // Set priority for TSOP
         IPC2bits.INT2IS = 3; // Set subpriority for TSOP
-        IFS0bits.INT2IF = 0; // Clear the int2
+        IFS0bits.INT2IF = 0; // Clear the int2 flag
         IEC0bits.INT2IE = 1; // Enable interrupt for TSOP
 
-        //Let the Input Capture configure the timer; if it does it
         T3CONbits.TON = 0; // disable timer 3
         T2CONbits.TON = 0; // disable timer 2
         T2CONbits.T32 = 1; // timer 2 and 3 as one 32 bits timer (instead of 2 16 bits timers)
-	PR2 = 40 * 67500; // timer @40MHz -> 40 clock pulse / microseconds. // Tmr period = signal (fixed) length
+	PR2 = 40 * 68160; // timer @40MHz -> 40 clock pulse / microseconds. // Tmr period = signal (fixed) length
         T2CONbits.TON = 1; // enable timer 2
         IEC0bits.T3IE = 1; // enable timer 1 interrupt
         IFS0bits.T3IF = 0; // clear timer 1 interrupt
