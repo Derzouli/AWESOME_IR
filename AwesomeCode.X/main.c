21b47fee2010b77773e54ea42378220467709e06
lpoujade <lpoujade@student.42.fr>
2016-05-26 16:33:32 +0200

    base d une facon non-bloquante de recuperer la trame.
    chqaue falling edge est enregistre dans reception[X],
    x partant de -2 pour eviter le header
    lorsque X arrive a 32, la trame est passee : reste a
    passer en binaire (je suis pas trop sur de mes operateurs binaires)
    
    /!\  l envoi via l uart va bloquer l interrupt
    /!\  si un seul signal de repeat est envoye par la telecommande, ca va
    tout decaler definitivement
    
    /!\ ca ne fonctionne pas

--- AwesomeCode.X/main.c
+++ AwesomeCode.X/main.c
@@ -9,6 +9,7 @@
 # include "fcts.h"
 
 u32 reception[32];
+s8 c = -4;
 
 // value for NEC
 // 560 usec
@@ -24,31 +25,10 @@ u32 reception[32];
 	__attribute__((interrupt(IPL4AUTO), nomips16, vector(_EXTERNAL_2_VECTOR)))
 void			ir_receive(void)
 {
-    u8 c = 0;
-    u32 tmr_tmp;
-
-    IFS0bits.T3IF = 0;
-    TMR2 = 0;
-    LATFbits.LATF1 = 1;
-    while (!PORTDbits.RD9)  // wait the header (1st bit, low signal)
-        ;
-    LATFbits.LATF1 = 0;
-    while (PORTDbits.RD9)   // header
-        ;
-    LATFbits.LATF1 = 1;
-    while (IFS0bits.T3IF == 0)  // lets get the trame
-    {
-        while (!PORTDbits.RD9)
-            ;
-        LATFbits.LATF1 = 0;
-        tmr_tmp = TMR2;
-        while (PORTDbits.RD9)
-            ;
-        LATFbits.LATF1 = 1;
-        reception[c] = TMR2 - tmr_tmp;
-        c++;
-    }
-    LATFbits.LATF1 = 0;
+    LATFbits.LATF1 ^= 1;
+    if (c >= 0)
+        reception[c] = TMR2;
+    c++;
     IFS0bits.INT2IF = 0;
 }
 
@@ -66,11 +46,12 @@ int main(void)
         IPC2bits.INT2IS = 3; // Set subpriority for TSOP
         IFS0bits.INT2IF = 0; // Clear the int2 flag
         IEC0bits.INT2IE = 1; // Enable interrupt for TSOP
+        INTCONbits.INT2EP = 0;
 
-        T3CONbits.TON = 0; // disable timer 3
         T2CONbits.TON = 0; // disable timer 2
         T2CONbits.T32 = 1; // timer 2 and 3 as one 32 bits timer (instead of 2 16 bits timers)
-	PR2 = 40 * 68160; // timer @40MHz -> 40 clock pulse / microseconds. // Tmr period = signal (fixed) length
+	PR2 = 0b1000000100000000; // wweak bits timer @40MHz -> 40 clock pulse / microseconds. // Tmr period = signal (fixed) length
+        PR3 = 0b0000000000101001; // Most significant bits
         T2CONbits.TON = 1; // enable timer 2
         IEC0bits.T3IE = 1; // enable timer 1 interrupt
         IFS0bits.T3IF = 0; // clear timer 1 interrupt
@@ -78,7 +59,57 @@ int main(void)
         asm("ei");		// enable interrupts
 
 	uart_init(9600, 40000000);
+        u8 y, i;
+        u32 trame = 0b0;
 	while (1)
-            ;
+        {
+            if (c >= 32)
+            {
+                IEC0bits.INT2IE = 0;
+                i = 0;
+                for (y = 0; y < 32; y++)
+                {
+                    trame |= (reception[i + 1] - reception[i]) - 520 > 30000 ? (1 << y) : (0 << y);
+                    i += 2;
+                }
+                uart_putnbr(trame, 2); uart_putstr("\n\r");
+                TMR2 = 0;
+                c = -4;
+                IEC0bits.INT2IE = 1;
+            }
+        }
 	return (0);
 }
+
+//{
+//    u8 c = 0;
+//    u32 tmr_tmp;
+//
+//    TMR2 = 0;
+//    IFS0bits.T3IF = 0;
+//    LATFbits.LATF1 = 1;
+//
+//    while (!PORTDbits.RD9)  // wait the header (1st bit, low signal)
+//        ;
+//    while (PORTDbits.RD9)   // header
+//        ;
+//    if (TMR2 < (40 * 12000))
+//    {
+//        while (TMR2 < (13000 * 40))
+//            ;
+//        LATFbits.LATF1 = 0;
+//        return ;
+//    }
+//    while (c < 32)  // lets get the trame
+//    {
+//        while (!PORTDbits.RD9)  // part up; fixed length : verif here possible (but useful ?)
+//            ;
+//        tmr_tmp = TMR2;
+//        while (PORTDbits.RD9)
+//            ;
+//        reception[c] = TMR2 - tmr_tmp;
+//        c++;
+//    }
+//    LATFbits.LATF1 = 0;
+//    IFS0bits.INT2IF = 0;
+//}
